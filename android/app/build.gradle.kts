d07d2398d2f29c0506052cdeae39658fecc244ab
pradanadimas534 <pradanadimas534@gmail.com>
2026-05-21 20:44:32 +0700

    Fix: Update ndkVersion to a specific version in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.fluttter_app_casier"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
