92146c7aeaf1ae625514e96d10135909a18ccd28
pradanadimas534 <pradanadimas534@gmail.com>
2026-06-14 21:25:14 +0700

    refactor: remove unused Google Services dependency from build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,8 +22,3 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-buildscript {
-    dependencies {
-        classpath 'com.google.gms:google-services:4.4.2'
-    }
-}
